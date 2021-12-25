3ac2483c8ef82e42556e79d466f77ada4b4d46eb
yajie <909092921@qq.com>
2021-12-25 13:56:47 +0800

    完成打电话 和复制微信信息功能

--- miniprogram/pages/thingdetail/thingdetail.js
+++ miniprogram/pages/thingdetail/thingdetail.js
@@ -1,8 +1,13 @@
+let db = wx.cloud.database()
+let dbcollect = db.collection("collect")
+let app = getApp()
+let tools = require('../../tools/tools')
 Page({
-    data:{
+    data: {
+        thingid: "",
         username: "",
         userimg: "",
-        userconnect:"",
+        userconnect: "",
         thingname: "",
         thinginfo: "",
         thingclass: "",
@@ -13,25 +18,84 @@ Page({
         facetoface: "",
         nofacetoface: "",
     },
-    onLoad(option){
-        // 把字符串解析为对象
-        let objInfo = JSON.parse(option.info)
-        console.log(objInfo)
-        this.setData({
-            username: objInfo.username,
-            userimg: objInfo.userimg,
-            userconnect: objInfo.userconnect,
-            userphone: objInfo.userphone,
-            thingname: objInfo.thingname,
-            thinginfo: objInfo.thinginfo,
-            thingclass: objInfo.thingclass,
-            thingprice: objInfo.thingprice,
-            thingoriginprice: objInfo.thingoriginprice,
-            thingtranprice: objInfo.thingtranprice,
-            thingimg: objInfo.thingimg,
-            facetoface: objInfo.facetoface,
-            nofacetoface: objInfo.nofacetoface,
+    // 复制微信号
+    makeCopy(event){
+        if(this.data.userconnect != "")
+        {
+            wx.setClipboardData({
+                data: this.data.userconnect,
+                success: res =>{
+                    tools.showRightToast("复制成功!")
+                },
+                fail: res =>{
+                    tools.showErrorToast("用户未上传")
+                }
+              })
+        }else{
+            tools.showErrorToast("该用户没有上传微信号")
+        }
+    },
+    //打电话
+    makeCall(event){
+            wx.makePhoneCall({
+                phoneNumber: this.data.userphone,
+                fail: res =>{
+                    tools.showErrorToast("用户未上传")
+                }
+            })
+    },
+    // 收藏
+    collectThing() {
+        dbcollect.where({
+            _openid: app.globaldata.openid,
+            thingid: this.data.thingid,
+        }).count({
+            success: res =>{
+                if (res.total == 0) {
+                    dbcollect.add({
+                        data: {
+                            thingid: this.data.thingid,
+                        },
+                        success: res => {
+                            tools.showRightToast("收藏成功")
+                        }
+                    })
+                } else {
+                    tools.showRightToast("宝贝已收藏")
+                }
+            }
         })
+    },
+    onLoad(option) {
+            // 把字符串解析为对象
+            let objInfo = JSON.parse(option.info)
+            console.log(objInfo)
+            this.setData({
+                thingid: objInfo._id,
+                username: objInfo.username,
+                userimg: objInfo.userimg,
+                userconnect: objInfo.userconnect,
+                userphone: objInfo.userphone,
+                thingname: objInfo.thingname,
+                thinginfo: objInfo.thinginfo,
+                thingclass: objInfo.thingclass,
+                thingprice: objInfo.thingprice,
+                thingoriginprice: objInfo.thingoriginprice,
+                thingtranprice: objInfo.thingtranprice,
+                thingimg: objInfo.thingimg,
+                facetoface: objInfo.facetoface,
+                nofacetoface: objInfo.nofacetoface,
+            })
         console.log(this.data)
+    },
+  onShow() {
+    if (app.globaldata.openid == "") {
+      tools.showErrorToast('请先登陆')
+      setTimeout(function () {
+        wx.switchTab({
+          url: '/pages/user/user',
+        })
+      }, 1000)
     }
+  }
 })
